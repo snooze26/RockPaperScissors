2780ceda47eeca4b5358f02c1e0449b010507b01
snooze26 <langdon.douglas26@gmail.com>
2024-08-26 19:48:17 -0500

    Added getHuman, playRound, playGame functions

--- script.js
+++ script.js
@@ -7,5 +7,51 @@ function getComputerChoice(){
 const choice = getComputerChoice();
 console.log(choice);
 
-userchoice = prompt("Rock, Paper, Scissors?")
-console.log(userchoice);
+function getHumanChoice(){
+let userchoice = prompt("Rock, Paper, Scissors?")
+let userInput = userchoice.toLowerCase();
+
+console.log(userInput);
+return userInput;
+
+}
+
+let humanScore = 0;
+let computerScore = 0; 
+
+function playRound(humanChoice, computerChoice){
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    if(humanChoice === 'rock' && computerChoice === 'scissors'){
+        console.log('Rock beats scissors'); 
+
+        humanScore++
+    }else if(humanChoice === 'paper' && computerChoice === 'rock'){
+        console.log('Paper beats rock'); 
+
+        humanScore++
+
+    }else if(humanChoice === 'scissors' && computerChoice === 'paper'){
+        console.log('Scissors beats paper'); 
+
+        humanScore++
+    }else{
+        computerScore++
+    }
+   
+    console.log(humanScore);
+    console.log(computerScore)
+}
+
+
+function playGame(){
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+}
+
+playGame();
